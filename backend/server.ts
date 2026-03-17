2b7cd3e71c8581463a3e5fd9309da7a7d43105c7
Hayder <hayder.alhatemi@gmail.com>
2026-03-17 22:53:41 +0200

    feat: Add task update endpoint and fix server setup

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,9 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+//temporarly
+console.log('new server code running')
+//
 dotenv.config()
 
 connectDB()
@@ -17,6 +20,12 @@ app.use('/api/tasks', taskRoutes)
 app.get('/', (req: Request, res: Response) => {
   res.send('Student Task Manager API is running')
 })
+
+//temporarly
+app.put("/test-put", (req: Request, res: Response) => {
+  res.json({ message: "PUT works" })
+})
+//
 
 const PORT = process.env.PORT || 5000
 
